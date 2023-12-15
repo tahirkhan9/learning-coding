d74446c95839fd6c35419970871fb450b7daf1f0
Tahir khan <tahirdecoy786@gmail.com>
2023-12-16 00:32:29 +0530

    website soci

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-// prectice 
-for(let num=0; num<=100; num++){
-    if(num%2 === 0) 
-    //  {even number
-        console.log("num =", num);
-    } 
-}
+let fullName = prompt("Enter your fullname without spacing");
+
+let username =  "@" + fullName + fullName.lenght;
+console.log(username);
