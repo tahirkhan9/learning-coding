b70e056d425d0201857359830dd3797ca7053fbe
Tahir khan <tahirdecoy786@gmail.com>
2023-12-28 15:25:28 +0530

    add the clc and todo

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-let cities = ["delhi","mumbai","kanpur","lucknow"];
+let display = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button')
 
-for (let city of cities) {
-    console.log(city.toUpperCase());
-}
+let buttonsArray = Array.from(buttons);
+
+let string = '';
+
+buttonsArray.forEach(btn => {
+        btn.addEventListener('click',(e) => {
+        if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            display.value = string;
+        }
+        else if(e.target.innerHTML == 'AC'){
+            string = '';
+            display.value = ""
+
+        }
+        else if(e.target.innerHTML == '='){
+            string = eval(string);
+            display.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            display.value = string;
+        }
+    });
+});
+
+
